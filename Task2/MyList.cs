6db749118a1352db8f0589edd624e52ee36ce9f6
JuliaTrushkova <kosyuska@yandex.ru>
2022-02-05 23:19:37 +0300

    Add project files.

---
+++ Task2/MyList.cs
@@ -0,0 +1,28 @@
+﻿namespace Task2
+{
+    internal class MyList<T>
+    {
+        private T[] arrayT;
+
+        public MyList(int Length)
+        {
+            arrayT = new T[Length];
+        }
+
+        public T this[int index]
+        {
+            get { return arrayT[index]; }
+            set { arrayT[index] = value; }
+        }
+
+        public int Count
+        {
+            get { return arrayT.Length; }
+        }
+
+        public void Add(T element)
+        {
+            arrayT = arrayT.Append(element).ToArray();
+        }        
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task2;
+
+Console.WriteLine("Hello, World!");
+int length = 10;
+MyList<int> myList = new MyList<int>(length);
+
+Random random = new Random();
+
+for (int i = 0; i < myList.Count; i++)
+{
+    myList[i] = random.Next(1, 100);
+}
+int addNum = 0;
+myList.Add(addNum);
+
+Console.WriteLine(myList.Count);
+
+for (int i = 0; i < myList.Count; i++)
+{
+    Console.WriteLine(myList[i].ToString());
+}
