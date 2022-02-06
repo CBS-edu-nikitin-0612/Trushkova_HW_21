cbe436e9f8e6691c90bd59baa1ff06c3aae4a460
JuliaTrushkova <97622315+JuliaTrushkova@users.noreply.github.com>
2022-02-06 22:12:06 +0300

    fix FactoryMethod

--- AddTask/MyClass.cs
+++ AddTask/MyClass.cs
@@ -1,12 +1,8 @@
 ﻿namespace AddTask
 {
-    internal class MyClass<T>
+    internal class MyClass<T> where T : new()
     {
         //Фабричный метод для создания экземпляра по умолчанию класса Т
-        public static T? FacrotyMethod()
-        {
-            T? variable = default;
-            return variable;
-        }
+        public static T FactoryMethod() => new T();       
     }
 }
