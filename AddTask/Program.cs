6c91542622a0c0052225f5a044d30492e08e3613
JuliaTrushkova <97622315+JuliaTrushkova@users.noreply.github.com>
2022-02-06 22:49:37 +0300

    fix Program.cs in AddTask

--- AddTask/Program.cs
+++ AddTask/Program.cs
@@ -4,24 +4,14 @@ using AddTask; //без явного указания не работает пр
 Console.WriteLine("Hello, Teacher!");
 
 //Создание и вывод на экран переменной по умолчанию типа int
-int? variable1 = MyClass<int>.FacrotyMethod();
+int variable1 = MyClass<int>.FacrotyMethod();
 Console.WriteLine("int " + variable1);
 
-//Создание и вывод на экран переменной по умолчанию типа string
-string? variable2 = MyClass<string>.FacrotyMethod();
-Console.WriteLine("string " + variable2);
-if (variable2 == string.Empty)
-    Console.WriteLine("string Empty");
-if (variable2 == "")
-    Console.WriteLine("string WhiteSpace");
-if (variable2 == null)
-    Console.WriteLine("string null");
-
 //Создание и вывод на экран переменной по умолчанию типа decimal
-decimal? variable3 = MyClass<decimal>.FacrotyMethod();
+decimal variable3 = MyClass<decimal>.FacrotyMethod();
 Console.WriteLine("decimal " + variable3);
 
 //Создание и вывод на экран переменной по умолчанию типа bool
-bool? variable4 = MyClass<bool>.FacrotyMethod();
+bool variable4 = MyClass<bool>.FacrotyMethod();
 Console.WriteLine("bool " + variable4);
 
